9465b0bfe88813f47506ce2cdad9f49724d9dc4a
elly1107 <elly1107011@gmail.com>
2024-07-11 22:29:42 +0900

    java-basic

---
+++ src/access/a/AccessData.java
@@ -0,0 +1,21 @@
+package access.a;
+
+public class AccessData {
+    public int publicField;
+    int defaultField;
+    private int privateField;
+
+    public void publicMethod(){
+        System.out.println("publicMethod 호출"+ publicField);
+    }
+
+    void defaultMethod(){
+        System.out.println("defaultMethod 호출 "+ defaultField);
+    }
+
+    private void privateMethod(){
+        System.out.println("privateMethod 호출 "+ privateField);
+    }
+
+
+}
