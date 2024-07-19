45c79724665b03601afc0a2178cfb7495841d72a
elly1107 <elly1107011@gmail.com>
2024-07-19 13:53:06 +0900

    java-basic

--- src/final1/FinalLocalMain.java
+++ src/final1/FinalLocalMain.java
@@ -3,6 +3,16 @@ package final1;
 public class FinalLocalMain {
     public static void main(String[] args) {
         //final 지역 변수
+        final int data1;
 
+        data1=10; //최초 한번만 할당 가능
+        //data1=20; // 두번째 할당부터는 오류 발생
+
+        final int data2=10;
+        //data2=20; compile error
+    }
+
+    static void method(final int parameter) {
+        //parameter=20; compile error
     }
 }
